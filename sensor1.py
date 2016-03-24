1186e585f99a8c45e25a1e51628a9e16a3a707a0
austinmeier <austin.e.meier@gmail.com>
2016-03-23 23:30:06 -0700

    Added a template for python codes that will hopefully be part of my package

---
+++ sensor1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+#####################################################################
+#   code developed by: austinmeier
+#   developed on: 03/24/16
+#   contact:austinmeier on github
+#####################################################################
+#                       Imports here
+#####################################################################
+import time
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+
+
+
+
+"""
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
+
+while True:
+    if (GPIO.input(23) == False):
+        os.system('mpg123 -q binary-language-moisture-evaporators.mp3 &amp;')
+
+    if (GPIO.input(24) == False):
+        os.system('mpg123 -q power-converters.mp3 &amp;')
+
+    if (GPIO.input(25)== False):
+        os.system('mpg123 -q vader.mp3 &amp;')
+
+    sleep(0.1);
+"""
+
+
+
+
+
+
+
+#####################################################################
+#                       Define functions
+#####################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################################################
+#                     Put code below this
+#####################################################################
+
