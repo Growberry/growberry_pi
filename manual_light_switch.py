5efff3a4aa5023ac7c3a598654d7312ebe30063c
austinmeier <austin.e.meier@gmail.com>
2016-03-25 00:05:05 -0700

    added a 3rd light, yellow.  Also added a second argument to the flash option.  Will flash n number of times if input is "flash n"

--- manual_light_switch.py
+++ manual_light_switch.py
@@ -16,9 +16,12 @@ import RPi.GPIO as GPIO
 #                           Variables
 #####################################################################
 
-
-
-
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+GPIO.setwarnings(True)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.setup(18,GPIO.OUT)
 
 
 #####################################################################
@@ -26,7 +29,6 @@ import RPi.GPIO as GPIO
 #####################################################################
 
 #####################################################################
-GPIO.setmode(GPIO.BCM)
 
 def main():
     color= (light_chose())
@@ -35,93 +37,110 @@ def main():
         red(behavior)
     elif color == "green":
         green(behavior)
-    elif color == "both":
+    elif color == "yellow":
+        yellow(behavior)
+ 
+    elif color == "all":
         red(behavior)
         green(behavior)
-
+        yellow(behavior)
 
 
 
 
 def light_chose():
-    light=str(raw_input("Which light would you like to modify? (red/green)"))
-    #if light != "red" or light != "green":
-    #    print light
-    #    print('Sorry, that color choice is not recognized.  Please try again.')
-    #    light_chose()
-    #else: return light
-    return light
+    color_options = ["red","green","yellow","all"]
+    light=str(raw_input("Which light would you like to modify? (red/green/yellow/all)"))
+    if light not in color_options:
+        print('Sorry, that color choice is not recognized.  Please try again.')
+        light_chose()
+    else: return light
 
 
 def onoff(color):
-    choice=raw_input("Would you like the %s light on or off?"%(color))
-    #if choice != "on" or choice != "off":
-    #    print("Sorry, that behavior choice is not recognized.  Please try again.")
-    #    onoff(color)
-    #else: return choice
-    return choice
+    behavior_options = ["on","off","flash"]
+    
+    choice = [str(x) for x in raw_input("How would you like the %s light to behave? (on, off, flash x)"%(color)).split()]
+    print(choice)
+    if choice[0] not in behavior_options:
+        print("Sorry, that behavior choice is not recognized.  Please try again.")
+        onoff(color)
+    else: return choice
 
 
 
 
 
 def red(x):
-    if x == "on":
+    print x
+    if x[0] == "on":
         print("Red light is ON.")
         GPIO.output(27,GPIO.HIGH)
-    elif x == "off":
+    elif x[0] == "off":
         print("Red light is OFF.")
         GPIO.output(27,GPIO.LOW)
-    elif x == "flash":
-        print("Red light is flashing")
-        GPIO.output(27,GPIO.HIGH)   #on
-        time.sleep(.5)              #remain on
-        GPIO.output(27,GPIO.LOW)    #off
-        time.sleep(1)
-        GPIO.output(27,GPIO.HIGH)   #on
-        time.sleep(.5)              #remain on
-        GPIO.output(27,GPIO.LOW)    #off
-        time.sleep(1)
-        GPIO.output(27,GPIO.HIGH)   #on
-        time.sleep(.5)              #remain on
-        GPIO.output(27,GPIO.LOW)    #off
-
+    elif x[0] == "flash":
+        flashnumber = 3
+        if len(x)>1:
+            print ("Red light will flash %s times."%x[1])
+            flashnumber = int(x[1])
+        else:
+            print("Red light will flash 3 times.")
+        for i in range(flashnumber):
+            time.sleep(.5)
+            GPIO.output(27,GPIO.HIGH)   #on
+            time.sleep(.5)              #remain on
+            GPIO.output(27,GPIO.LOW)    #off
         
 
 def green(x):
-    if x == "on":
+    print x
+    if x[0] == "on":
         print("Green light is ON.")
         GPIO.output(17,GPIO.HIGH)
-    elif x == "off":
+    elif x[0] == "off":
         print("Green light is OFF.")
         GPIO.output(17,GPIO.LOW)
-    elif x == "flash":
-        print("Green light is flashing")
-        GPIO.output(17,GPIO.HIGH)   #on
-        time.sleep(.5)              #remain on
-        GPIO.output(17,GPIO.LOW)    #off
-        time.sleep(1)
-        GPIO.output(17,GPIO.HIGH)   #on
-        time.sleep(.5)              #remain on
-        GPIO.output(17,GPIO.LOW)    #off
-        time.sleep(1)
-        GPIO.output(17,GPIO.HIGH)   #on
-        time.sleep(.5)              #remain on
-        GPIO.output(17,GPIO.LOW)    #off
+    elif x[0] == "flash":
+        flashnumber = 3
+        if len(x)>1:
+            print ("Green light will flash %s times."%x[1])
+            flashnumber = int(x[1])
+        else:
+            print("Green light will flash 3 times.")
+        for i in range(flashnumber):
+            time.sleep(.5)
+            GPIO.output(17,GPIO.HIGH)   #on
+            time.sleep(.5)              #remain on
+            GPIO.output(17,GPIO.LOW)    #off
+        
 
-
-
+def yellow(x):
+    print x
+    if x[0] == "on":
+        print("Yellow light is ON.")
+        GPIO.output(18,GPIO.HIGH)
+    elif x[0] == "off":
+        print("Yellow light is OFF.")
+        GPIO.output(18,GPIO.LOW)
+    elif x[0] == "flash":
+        flashnumber = 3
+        if len(x)>1:
+            print ("Yellow light will flash %s times."%x[1])
+            flashnumber = int(x[1])
+        else:
+            print("Yellow light will flash 3 times.")
+        for i in range(flashnumber):
+            time.sleep(.5)
+            GPIO.output(18,GPIO.HIGH)   #on
+            time.sleep(.5)              #remain on
+            GPIO.output(18,GPIO.LOW)    #off
+        
 
 
 #####################################################################
 #                     Put code below this
 #####################################################################
-GPIO.setmode(GPIO.BCM)
-GPIO.cleanup()
-GPIO.setwarnings(True)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
-
 
 
 while True:
