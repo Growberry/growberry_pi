4490d29ba480cc28cf2857e7ca77592580f6b752
austinmeier <meiera@science.oregonstate.edu>
2016-10-01 00:15:18 -0700

    Updated the GET api test script

--- test_api_GET.py
+++ test_api_GET.py
@@ -3,5 +3,7 @@ import time
 
 while True:
     r = requests.get('http://192.168.0.42:8000/get_settings/10')
-    print r.headers
-    time.sleep(3)
+    headers = str(r.headers)
+    with open('/home/pi/API_test.txt', 'w') as outfile:
+		outfile.write(headers)
+    time.sleep(1800)
