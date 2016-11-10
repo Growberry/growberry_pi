2064afe3ddf27c42bfb1fbdf8de307be1d036116
austinmeier <meiera@science.oregonstate.edu>
2016-11-09 21:21:09 -0800

    added safetyvalve to sun.py

--- growberry/sun.py
+++ growberry/sun.py
@@ -1,22 +1,31 @@
 import datetime
+from one_wire_temp import w1therm
+from threading import Thread
 
 class Sun:
-
     """the sun class handles all tasks related to turning the lights on and off"""
+    def safetyvalve(self,lights):
+        heatsinktemps = w1therm()
+        while True:
+            temps = heatsinktemps.gettemps()
+            for temp in temps:
+                # check if heatsinks are hotter than 50, if so, turn the lights off!
+                if temp > 20:
+                    lights.off()
+                    # somehow notify the user.. email maybe?
+            sleep(10)
+
+
     def __init__(self,lights,settings):
         self.settings = settings # this is a Settings class
         self.lights = lights # Relay class
-        # start monitoring heatsinks:
-        # heatsinktemps = w1therm()
-        # while True:
-        #     temps = heatsinktemps.gettemps()
-        #     for temp in temps:
-        #         # check if heatsinks are hotter than 50, if so, turn the lights off!
-        #         if temp > 50:
-        #             lights.off()
-        #             # somehow notify the user.. email maybe?
-        #     sleep(10)
+        
+        # start monitoring heatsinks
+        t1 = Thread(target = self.safetyvalve, args = self.lights)
+        t1.start()
+
     def lightcontrol(self):
+        # see if the lights should be on/off, and make them that way
         sunrise = datetime.datetime.combine(datetime.date.today(), self.settings.sunrise)
         sunset = sunrise + self.settings.daylength
 
@@ -26,7 +35,7 @@ class Sun:
             self.lights.off()
 
 
-        # see if the lights should be on/off, and make them that way
+
 
         # monitor heatsink temps, shut off when too hot
 
