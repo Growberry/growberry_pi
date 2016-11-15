82e4906a1f3819842ec305c33a0ed6eb175c7b09
austinmeier <meiera@science.oregonstate.edu>
2016-11-14 20:17:49 -0800

    wind.py now works as intended.

--- growberry/wind.py
+++ growberry/wind.py
@@ -4,36 +4,36 @@ GPIO.setwarnings(True)  # set to false if the warnings bother you, helps trouble
 
 class Wind:
     """This will house all of functions used to control the fans"""
-    def __init__(self, powerpin, speedpin, speed = 0):  # add lights, if need be
+    def __init__(self, powerpin, speedpin, speed = 0):
         self.powerpin = powerpin
         GPIO.setup(powerpin, GPIO.OUT, initial=1)
         GPIO.setup(speedpin, GPIO.OUT)
         self.pwm = GPIO.PWM(speedpin, 25000) # 25 Kilohertz is inaudible to human ears
-        #self._speed = speed
         self.pwm.start(speed)
         self.tach = speed
-#        self.lights = lights
+        # self.lights = lights
 
 
     def speed(self, value):
         """ handles the speed of the fans, including master power, on/off."""
+        dutycycle = 100.0 - value  # 100 is slow, 0 is fast
         if 0 < value <= 100:
-            self.pwm.ChangeDutyCycle(value)
+            self.pwm.ChangeDutyCycle(dutycycle)
             # GPIO.output(self.powerpin, GPIO.LOW) # power on
             self.on()
             self.tach = value
-            print "ON"
+            # print "ON"
         elif value == 0:
             self.off()
             self.tach = 0
-            print "OFF"
+            # print "OFF"
             # GPIO.output(self.powerpin, GPIO.HIGH)  # power off
             # if self.lights.state:  #lights.state = 1 means lights are off
             #     GPIO.output(self.powerpin, GPIO.HIGH)  # power off
             # else:
             #     raise ValueError("cannot set speed to 0 when lights are on")
         else:
-            raise ValueError("Speed must be between 0-100")
+            raise ValueError("Speed must be between 0.0-100.0")
 
     def on(self):
         """switches GPIO pin to LOW/0 - in open state relays, this turns the relay ON."""
@@ -60,10 +60,10 @@ if __name__ == "__main__":
             except ValueError:
                 print "invalid set speed"
             except:
-                break
                 print "broken"
+                break
             else:
-                print("New duty cycle = ", inputspeed)
+                print "New duty cycle = ", inputspeed
 
     finally:
         wind.pwm.stop()
